b20b44f033da5aa7e6cc610893affa3467803c2d
Thomaz Simão <thomazsimao5@gmail.com>
2023-01-17 11:11:03 -0300

    resultado final com o index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-const colors = [ red, blue , yellow , purple , green ]
+const colors = ['Red', 'Black', 'Blue', 'Yellow', 'Purple', 'Green']
+const btn = document.getElementById('btn')
+const colorName = document.querySelector('.color') // pegando a classe para associar no text 
+// usando o metodo query     
+
+// no click 
+btn.addEventListener('click', function () {
+    const randomColor = getRandonColor() // associando a variavel 
+    document.body.style.background = colors[randomColor]  //aplicando a cor no Background
+    colorName.textContent = colors[randomColor] //inserindo a cor associada 
+
+    //console.log(randomColor)
+})
+
+// Gerando um numero randomico
+
+function getRandonColor() {
+    return Math.floor(Math.random() * colors.length)
+}
+
